60770fcefe17f65a09183979ae9f428d4fdc547e
Ahmad Santarissy <ahmad.s@cgiandi.com>
2019-03-15 20:44:22 +0200

    finished index.js configs

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 module.exports = {
-  globals: {},
-  rules: {},
+  extends: ['eslint:recommended', 'eslint-config-airbnb-base'].map(
+    require.resolve,
+  ),
+  plugins: ['import', 'node', 'promise'],
+  globals: {
+    require: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    strict: 'error',
+  },
 };
